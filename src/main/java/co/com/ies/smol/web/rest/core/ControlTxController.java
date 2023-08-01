da6f49e3f6faa46a2491885b19b7c2e95897a2a4
Aristides Fernandez <tidefdezr@gmail.com>
2023-08-01 16:31:48 -0500

    feat ControlTxController : se agrega la anotación @Valid para validar las restricciones de los DTO de la API.

--- src/main/java/co/com/ies/smol/web/rest/core/ControlTxController.java
+++ src/main/java/co/com/ies/smol/web/rest/core/ControlTxController.java
@@ -4,9 +4,13 @@ import co.com.ies.smol.service.core.ControlTxService;
 import co.com.ies.smol.service.dto.core.AssignBoardDTO;
 import co.com.ies.smol.service.dto.core.BoardRegisterDTO;
 import java.net.URISyntaxException;
+import java.util.Objects;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +29,16 @@ public class ControlTxController {
     }
 
     @PostMapping("/board/register")
-    public ResponseEntity<String> createBoardRegister(@RequestBody BoardRegisterDTO boardRegisterDTO) throws URISyntaxException {
+    public ResponseEntity<String> createBoardRegister(@Valid @RequestBody BoardRegisterDTO boardRegisterDTO, Errors errors)
+        throws URISyntaxException {
         log.debug("REST request to save BoardRegisterDTO : {}", boardRegisterDTO);
+
+        FieldError fieldError = errors.getFieldError();
+
+        if (errors.hasErrors() && Objects.nonNull(fieldError)) {
+            String errorMsg = fieldError.getField().concat(" ").concat(fieldError.getDefaultMessage());
+            return ResponseEntity.ok(errorMsg);
+        }
 
         controlTxService.createBoardRegister(boardRegisterDTO);
 
@@ -34,8 +46,16 @@ public class ControlTxController {
     }
 
     @PostMapping("/assign/board")
-    public ResponseEntity<String> assignInterfaceBoard(@RequestBody AssignBoardDTO assignBoardDTO) throws URISyntaxException {
+    public ResponseEntity<String> assignInterfaceBoard(@Valid @RequestBody AssignBoardDTO assignBoardDTO, Errors errors)
+        throws URISyntaxException {
         log.debug("REST request to save assignInterfaceBoard : {}", assignBoardDTO);
+
+        FieldError fieldError = errors.getFieldError();
+
+        if (errors.hasErrors() && Objects.nonNull(fieldError)) {
+            String errorMsg = fieldError.getField().concat(" ").concat(fieldError.getDefaultMessage());
+            return ResponseEntity.ok(errorMsg);
+        }
 
         controlTxService.assignInterfaceBoard(assignBoardDTO);
 
