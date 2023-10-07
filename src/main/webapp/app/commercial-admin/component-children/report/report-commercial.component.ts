1c29ed0dbbf265659e737531b2aa10dbb42a0cbc
Aristides Fernandez <tidefdezr@gmail.com>
2023-10-07 12:09:39 -0500

    feat(front): paginación del reporte comercial para la vista de las tarjetas en stock.

--- src/main/webapp/app/commercial-admin/component-children/report/report-commercial.component.ts
+++ src/main/webapp/app/commercial-admin/component-children/report/report-commercial.component.ts
@@ -1,5 +1,9 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ITEMS_PER_PAGE, TOTAL_COUNT_RESPONSE_HEADER } from 'app/config/pagination.constants';
+import { IInterfaceBoard } from 'app/entities/interface-board/interface-board.model';
+import { CommercialService } from './report-commercial.service';
 
 @Component({
   selector: 'jhi-report',
@@ -7,9 +11,43 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./report-commercial.component.scss'],
 })
 export class ReportCommercialComponent implements OnInit {
-  constructor(protected activatedRoute: ActivatedRoute, protected router: Router) {}
+  constructor(protected activatedRoute: ActivatedRoute, protected router: Router, protected commercialService: CommercialService) {}
 
-  ngOnInit(): void {}
+  interfaceBoards?: IInterfaceBoard[];
+  pageSize: number = ITEMS_PER_PAGE;
+  page = 1;
+  totalItems = 0;
 
-  public filter(): void {}
+  ngOnInit(): void {
+    this.getBoardsInStock();
+  }
+
+  getBoardsInStock(): void {
+    this.commercialService.getBoardsInStock(this.page - 1, this.pageSize).subscribe(res => {
+      this.onSuccess(res.body!, res.headers);
+    });
+  }
+
+  private onSuccess(interfaceBoards: IInterfaceBoard[], headers: HttpHeaders): void {
+    this.totalItems = Number(headers.get(TOTAL_COUNT_RESPONSE_HEADER));
+    this.interfaceBoards = interfaceBoards;
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.pageSize);
+  }
+
+  nextPage() {
+    if (this.page < this.totalPages) {
+      this.page++;
+      this.getBoardsInStock();
+    }
+  }
+
+  prevPage() {
+    if (this.page > 1) {
+      this.page--;
+      this.getBoardsInStock();
+    }
+  }
 }
--- src/main/webapp/app/commercial-admin/component-children/report/report-commercial.service.ts
+++ src/main/webapp/app/commercial-admin/component-children/report/report-commercial.service.ts
@@ -1,10 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApplicationConfigService } from 'app/core/config/application-config.service';
+import { IInterfaceBoard } from 'app/entities/interface-board/interface-board.model';
+import { Observable } from 'rxjs/internal/Observable';
+
+export type EntityArrayResponseType = HttpResponse<IInterfaceBoard[]>;
 
 @Injectable({ providedIn: 'root' })
 export class CommercialService {
-  protected resourceUrlApiCommercial = this.applicationConfigService.getEndpointFor('api/commercial');
-
+  protected resourceUrl = this.applicationConfigService.getEndpointFor('api');
   constructor(protected http: HttpClient, protected applicationConfigService: ApplicationConfigService) {}
+
+  getBoardsInStock(page: number, pageSize: number): Observable<HttpResponse<IInterfaceBoard[]>> {
+    return this.http.get<IInterfaceBoard[]>(`${this.resourceUrl}/info/boards/available?page=${page}&size=${pageSize}`, {
+      observe: 'response',
+    });
+  }
 }
