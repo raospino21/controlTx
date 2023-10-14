66f696bd8112dbcb63b8c9bb6117c15f38314132
Aristides Fernandez <tidefdezr@gmail.com>
2023-10-13 19:02:24 -0500

    refactor(commercial): se elimina el componente de tarjetas en stock en el modulo comercial.

--- src/main/webapp/app/commercial-admin/commercial-admin.module.ts
+++ src/main/webapp/app/commercial-admin/commercial-admin.module.ts
@@ -4,11 +4,10 @@ import { SharedModule } from 'app/shared/shared.module';
 
 import { CommercialComponent } from './commercial-admin.component';
 import { COMMERCIAL_ROUTE } from './commercial-admin.route';
-import { BoardInStockComponent } from './component-children/board/board-in-stock.component';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forRoot([COMMERCIAL_ROUTE], { useHash: true })],
-  declarations: [CommercialComponent, BoardInStockComponent],
+  declarations: [CommercialComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CommercialAdminModule {}
--- src/main/webapp/app/commercial-admin/component-children/commercial-children-routing.module.ts
+++ src/main/webapp/app/commercial-admin/component-children/commercial-children-routing.module.ts
@@ -1,21 +1,11 @@
 import { Injectable, NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 import { UserRouteAccessService } from 'app/core/auth/user-route-access.service';
-import { BoardInStockComponent } from './board/board-in-stock.component';
 
 @Injectable({ providedIn: 'root' })
 export class CommercialChildren {}
 
-export const COMMERCIAL_LIST_ROUTE: Route = {
-  path: 'board-in-stock',
-  component: BoardInStockComponent,
-  data: {
-    authorities: ['ROLE_COMMERCIAL'],
-    defaultSort: 'id,asc',
-    pageTitle: 'commercial-admin.home.title',
-  },
-  canActivate: [UserRouteAccessService],
-};
+export const COMMERCIAL_LIST_ROUTE: Route = {};
 
 const ROUTES: Routes = [COMMERCIAL_LIST_ROUTE];
 
