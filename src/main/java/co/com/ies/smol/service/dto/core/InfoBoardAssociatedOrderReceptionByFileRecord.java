4e0c8daf2305d9c54e8cac3814950bee316d5e3d
Janner93 <janner.gamarra@iesonline.com.co>
2023-11-24 09:22:22 -0500

    feat(InfoBoardAssociatedOrderReceptionByFileRecord): se crea nuevo record para la descarga de Mac en la orden de recepción.

---
+++ src/main/java/co/com/ies/smol/service/dto/core/InfoBoardAssociatedOrderReceptionByFileRecord.java
@@ -0,0 +1,7 @@
+package co.com.ies.smol.service.dto.core;
+
+import java.io.Serializable;
+
+public record InfoBoardAssociatedOrderReceptionByFileRecord(String mac, String validada) implements Serializable {
+    private static final long serialVersionUID = 5787683917777156451L;
+}
